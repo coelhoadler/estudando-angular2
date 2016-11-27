5c1e02b3f667e07b98a33f1fa784548ae495219b
Adler Coelho Santos <adler@agilebank.com.br>
2016-11-27 10:24:25 -0200

    Adicionando o decorator Http, criando um ngFor.

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+    fotos: Object[] = [];
+
+    constructor(http: Http) {
+
+        //rxjs = reactive extension for javascript
+        http
+        .get('v1/fotos')
+        .map( res => res.json())
+        .subscribe( fotos => {
+            this.fotos = fotos;
+        }, erro => console.log(erro));
+
+    }
+
+}
